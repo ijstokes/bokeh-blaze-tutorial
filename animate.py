6efbd376d011d14a1e09611d8ad04ac47d72a420
damianavila <damianavila@gmail.com>
2016-01-13 10:32:46 -0300

    Fixes to work with 0.11.0

--- animate.py
+++ animate.py
@@ -1,16 +1,15 @@
 import numpy as np
 import netCDF4
 
-from bokeh.plotting import vplot, hplot, cursession, output_server, show
+from bokeh.client import push_session
+from bokeh.io import curdoc
+from bokeh.plotting import vplot, hplot
 
 from viz import climate_map, timeseries, legend, get_slice  
 
 # Data
 data = netCDF4.Dataset('data/Land_and_Ocean_LatLong1.nc')
 t = data.variables['temperature']
-
-# Output option
-output_server("climate")
 
 # Plots
 climate_map = climate_map()
@@ -21,20 +20,21 @@ legend = legend()
 map_legend = hplot(climate_map, legend)
 layout = vplot(map_legend, timeseries)
 
-# Show
-show(layout)
+# open a session to keep our local document in sync with server
+session = push_session(curdoc())
 
 # Select data source for climate_map
 renderer = climate_map.select(dict(name="image"))
 ds = renderer[0].data_source
 
-# Create an infite loop to update image data
-import time
-
-while True:
+def update():
    for year_index in np.arange(2000, 2015, 1):
         for month_index in np.arange(1, 13, 1):
             image = get_slice(t, year_index, month_index)
             ds.data["image"] = [image]
-            cursession().store_objects(ds)
-            time.sleep(0.2)
+
+curdoc().add_periodic_callback(update, 20)
+
+session.show() # open the document in a browser
+
+session.loop_until_closed() # run forever
